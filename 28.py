545a46e62a7efc93f6ffae860f9e9512d58d2c3f
TsaiHUNGYU <eric585361@gmail.com>
2021-05-07 20:48:10 +0800

    almost there!

---
+++ 28.py
@@ -0,0 +1,21 @@
+a=list(input("輸入答案(4位數):"))
+b=list(input("please enter your answer :"))
+A=0
+B=0
+while True:
+    
+    if b == "0000":
+        break 
+    else:   
+        for i in range(len(a)):
+            for y in range(len(b)):
+                if b[i]==a[y] and i==y :
+                    A+=1
+                    break
+                elif b[i]==a[y] and i!=y:
+                    B+=1
+                    break
+    print(A,"A",B,"B")
+    A = 0
+    B = 0
+    b=list(input("please enter your answer :"))
